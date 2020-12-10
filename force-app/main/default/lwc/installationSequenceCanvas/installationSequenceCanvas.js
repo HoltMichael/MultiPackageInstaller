4ac1d023db961d80cf47f3a70f6245edfe135cdc
Michael Holt <michael.holt@michaelhol-ltm1.internal.salesforce.com>
2020-12-10 11:30:33 +0000

    Tidying Comments

--- force-app/main/default/lwc/installationSequenceCanvas/installationSequenceCanvas.js
+++ force-app/main/default/lwc/installationSequenceCanvas/installationSequenceCanvas.js
@@ -3,9 +3,6 @@ import { getRecord } from 'lightning/uiRecordApi';
 const FIELDS = ['MHolt__Package_Definition__c.MHolt__Namespace__c', 'MHolt__Package_Definition__c.MHolt__Package_Version__c', 'MHolt__Package_Definition__c.MHolt__Password__c'];
 
 export default class InstallationSequenceCanvas extends LightningElement {
-    @track col1Items = [{id: '1'}, {id: '2'}, {id:'3'}]
-    @track col2Items = [{id: '4'}, {id: '5'}, {id:'666666'}]
-    @track col3Items = [{id: '7'}, {id: '8'}, {id:'9'}]
     @track columns = {};
     @track showNewPackageModal = false;
     @track currentPackageDefRecordId;
@@ -21,7 +18,7 @@ export default class InstallationSequenceCanvas extends LightningElement {
             }
             this.dispatchEvent(
                 new ShowToastEvent({
-                    title: 'Error loading contact',
+                    title: 'Error loading package',
                     message,
                     variant: 'error',
                 }),
@@ -34,52 +31,93 @@ export default class InstallationSequenceCanvas extends LightningElement {
     //currentPackageDef;
 
     connectedCallback(){
-        //this.columns = [{id: '1', items: this.col1Items}, {id: '2', items: this.col2Items}, {id: '3', items: this.col3Items}];
         this.columns = [{id: '1', items: []}];
     }
 
+    /*
+        handleOndragstart
+        Takes the details of the current item and column being dragged and adds it to 
+        the dataTransfer 
+    */
     handleOndragstart(event) {
         event.target.style.opacity = 0.5;
         const dataTransfer = JSON.stringify({id: event.target.dataset.id, column: event.target.dataset.column});
         event.dataTransfer.setData("text/plain", dataTransfer);
     }
 
+    /*
+        handleOndragover
+        Stop default dragover functionality. Without this, the items
+        will return to their previous position ondrop
+    */
     handleOndragover(event) {
         event.preventDefault();
     }
 
+    /*
+        handleOndragenter
+        Make the dragged over object slightly see-through
+        when we drag over it
+    */
     handleOndragenter(event) {
         event.target.style.opacity = 0.5
     }
 
+    /*
+        handleOndragleave
+        Return the dragged over object's opacity to normal when
+        we exit the dragover
+    */
     handleOndragleave(event) {
         event.target.style.opacity = 1.0
     }
 
+    /*
+        handleAddPackage
+        Set the showNewPackageModal var to true
+        This brings up the createRecord cmp in a modal
+        for creating the package record
+    */
     handleAddPackage(){
         this.showNewPackageModal = true;
     }
 
+    /*
+        handleCreateNewPackage
+        Change the value of the current package definition record id
+        This will trigger an @wire call to get all the values we want for this record
+    */
     handleCreateNewPackage(event){
         this.currentPackageDefRecordId = event.detail;
         this.handleCloseModals();
     }
 
+    /*
+        addColumn
+        Adds a new row to the column object
+    */
     addColumn(){
         var columnId = this.columns.length + 1;
         this.columns.push({id: columnId, items: []});
     }
 
+    /*
+        addItem
+        Takes the details of a package and adds all information into the 
+        first row of the column object.
+    */
     addItem(packageDefinition){
-        console.log(packageDefinition);
         this.columns[0].items.push({id: this.currentPackageDefRecordId, 
                                     name: packageDefinition.fields.MHolt__Namespace__c.value,
                                     version: packageDefinition.fields.MHolt__Package_Version__c.value,
                                     password: packageDefinition.fields.MHolt__Password__c.value,
                                     col: 0});
-        console.log(this.columns[0].items);
     }
 
+    /*
+        handleCloseModals
+        Sets all modal vars to false in order to close them on-screen
+    */
     handleCloseModals(){
         this.showNewPackageModal = false;
     }
@@ -88,19 +126,32 @@ export default class InstallationSequenceCanvas extends LightningElement {
     /*
         getColumnIndexById    
         Takes an Id of an item in the columns and returns it's position
-        in the 
+        in the array
     */
     getColumnIndexById(id){
         var idFilter = (element) => element.id == id;
         return this.columns.findIndex(idFilter);
     }
 
+    /*
+        getItemIndexByValue
+        Takes an Id and an array, finds the index of the item in
+        the given array and returns the index
+    */
     getItemIndexByValue(id, obj){
         var idFilter = (element) => element.id == id;
         var x = obj.findIndex(idFilter);
         return obj.findIndex(idFilter);
     }
 
+    /*
+        handleOndrop
+        Handles the moving of a package between columns (or within the same column)
+        Takes the values of the dataTransfer provided in the handleOndragstart function
+        Removes the item from the dragged column, adds the item to the target column 
+        Shifts the other elements in that column accordingly, depending on where the item
+        was dropped
+    */
     handleOndrop(event) {
         event.currentTarget.style.opacity = 1.0;
         event.target.style.opacity = 1.0;
